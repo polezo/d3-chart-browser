924530eb254be315e65c60cd5c5aaec1845e273c
Adam Finch <atyreefinch@gmail.com>
2020-02-09 16:26:59 -0500

    github publish

---
+++ docs/precache-manifest.d5f1602b0fe5cf032044a1e0ca803bc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58c3e8b379ba05bdc26de25cbce21125",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80b301795abf15e4a893",
+    "url": "/static/css/main.f02f3a4f.chunk.css"
+  },
+  {
+    "revision": "4aca6e9c69331d467b14",
+    "url": "/static/js/2.9d5de1da.chunk.js"
+  },
+  {
+    "revision": "977b85ae92e0fb68aebdece70c5a2518",
+    "url": "/static/js/2.9d5de1da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80b301795abf15e4a893",
+    "url": "/static/js/main.bb93c45f.chunk.js"
+  },
+  {
+    "revision": "b1ed8156b5759df09602",
+    "url": "/static/js/runtime-main.7b2b068a.js"
+  }
+]);
